bf86d5e02c99a0c0710be7aef1e18bbe0a40b5f1
Nicol <nicolhawkins@yahoo.com>
2019-11-06 11:39:32 -0800

    Content directories were added to the variables index, about and resume

--- build.py
+++ build.py
@@ -2,16 +2,16 @@ top = open('templates/top.html').read()
 bottom = open('templates/bottom.html').read()
 
 
-content = open('index.html').read()
+content = open('content/index.html').read()
 index_html = top + content + bottom
 open('index.html', 'w+').write(index_html)
 
 
-content = open('about.html').read()
+content = open('content/about.html').read()
 about_html = top + content + bottom
 open('about.html', 'w+').write(about_html)
 
 
-content = open('resume.html').read()
+content = open('content/resume.html').read()
 resume_html = top + content + bottom
 open('resume.html', 'w+').write(resume_html)
