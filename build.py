28d4cc766d54c70ee979fc01b82933a508d1674b
Nicol <nicolhawkins@yahoo.com>
2019-11-05 21:27:23 -0800

    Added build.py script

---
+++ build.py
@@ -0,0 +1,17 @@
+top = open('top.html').read()
+bottom = open('bottom.html').read()
+
+
+content = open('index.html').read()
+index_html = top + content + bottom
+open('index.html', 'w+').write(index_html)
+
+
+content = open('about.html').read()
+about_html = top + content + bottom
+open('about.html', 'w+').write(about_html)
+
+
+content = open('resume.html').read()
+resume_html = top + content + bottom
+open('resume.html', 'w+').write(resume_html)
