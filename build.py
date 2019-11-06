9f47aa69053507c84f420ad1ad3f728867688b7e
Nicol <nicolhawkins@yahoo.com>
2019-11-06 14:40:58 -0800

    Needed to fix build.py for master branch too

--- build.py
+++ build.py
@@ -4,14 +4,14 @@ bottom = open('templates/bottom.html').read()
 
 content = open('content/index.html').read()
 index_html = top + content + bottom
-open('index.html', 'w+').write(index_html)
+open('docs/index.html', 'w+').write(index_html)
 
 
 content = open('content/about.html').read()
 about_html = top + content + bottom
-open('about.html', 'w+').write(about_html)
+open('docs/about.html', 'w+').write(about_html)
 
 
 content = open('content/resume.html').read()
 resume_html = top + content + bottom
-open('resume.html', 'w+').write(resume_html)
+open('docs/resume.html', 'w+').write(resume_html)
